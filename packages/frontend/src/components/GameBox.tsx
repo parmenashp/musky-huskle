380e831f06c7904c7796e9332b71b2aaced4ebb8
Pármenas Haniel <parmenashp@gmail.com>
2024-03-22 21:06:50 -0300

    Add avatar backdrop

--- packages/frontend/src/components/GameBox.tsx
+++ packages/frontend/src/components/GameBox.tsx
@@ -12,6 +12,13 @@ type BoxProps = {
 type BoxStyleProps = {
   avatar?: string;
 };
+const HoverTextStroke = styled(TextStroke)`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Box = styled.div<BoxStyleProps>`
   width: 5rem;
@@ -20,13 +27,13 @@ const Box = styled.div<BoxStyleProps>`
   justify-content: center;
   align-items: center;
   border: 0.313rem solid #000000;
-  z-index: -1;
+  z-index: 10;
 
   &.right {
     background-color: #40ac2e;
   }
   &.wrong {
-    background-color: #ac2e2e;
+    background-color: #bb2121;
   }
   &.partial {
     background-color: #c7b248;
@@ -34,10 +41,20 @@ const Box = styled.div<BoxStyleProps>`
   &.avatar {
     background-image: url(${(props) => props.avatar});
     background-size: cover;
+    background-repeat: no-repeat;
+
+    ${HoverTextStroke} {
+      opacity: 0;
+    }
+
+    &:hover {
+      ${HoverTextStroke} {
+        opacity: 1;
+        backdrop-filter: blur(0.125rem);
+      }
+    }
   }
 `;
-
-const HoverTextStroke = styled(TextStroke)``;
 
 function GameBox(props: BoxProps) {
   const { variant, avatar } = props;
@@ -46,7 +63,7 @@ function GameBox(props: BoxProps) {
     if (variant === "avatar") {
       return <HoverTextStroke>{props.children}</HoverTextStroke>;
     } else {
-      return <TextStroke>{props.children}</TextStroke>;
+      return <TextStroke strokeSize="3px">{props.children}</TextStroke>;
     }
   }
 
